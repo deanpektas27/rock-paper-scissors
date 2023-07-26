f03b70f213d1f1c9dd7cc6828c81c69288deb407
Dean Pektas <dean.pektas@svam.com>
2023-07-26 19:34:10 -0400

    Fixed logic of game code before creating UI

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -20,67 +20,61 @@ function playRound(playerSelection, computerSelection) {
     // make playerSelection case-INsensitive
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    console.log( 'Player: ' + playerSelection );
-    console.log( 'Computer: ' + computerSelection )
-    // make if statements for all scenarios
-    // Rock beats scissors
-    // Paper beats rock
-    // Scissors beat paper
-    // if player and computer choices are the same, go again
-    if(!(playerSelection == computerSelection)){
+    console.log(` Player: ${playerSelection} `);
+    console.log(` Computer: ${computerSelection} `);
+    if((playerSelection != computerSelection)){
         // if one player has rock and other scissors, rock wins
         if(playerSelection == 'rock' && computerSelection == 'scissors'){ 
-            win(playerSelection, computerSelection);
-        } else if(playerSelection == 'scissors' && computerSelection == 'rock'){ 
-            lose(playerSelection, computerSelection);
+            return win(playerSelection, computerSelection);
+        } 
+        else if(playerSelection == 'scissors' && computerSelection == 'rock'){ 
+            return lose(playerSelection, computerSelection);
         }
         // If one has paper and other rock, paper wins
         else if(playerSelection == 'paper' && computerSelection == 'rock'){ 
-            win(playerSelection, computerSelection);
+            return win(playerSelection, computerSelection);
         }
         else if(playerSelection == 'rock' && computerSelection == 'paper'){ 
-            lose(playerSelection, computerSelection);
+            return lose(playerSelection, computerSelection);
         }
         // If one has scissors and other paper, scissors wins
         else if(playerSelection == 'scissors' && computerSelection == 'paper'){ 
-            win();
-        } else if(playerSelection == 'paper' && computerSelection == 'scissors'){ 
-            lose();
+            return win(playerSelection, computerSelection);
+        } 
+        else if(playerSelection == 'paper' && computerSelection == 'scissors'){ 
+            return lose(playerSelection, computerSelection);
         }
     }
-    return "Tie! Play another round.";
+    else return "Tie! Play another round.";
 }
 
 function win(playerSelection, computerSelection) {
     playerScore++;
-    return "You Win! " + playerSelection + " beats " + computerSelection;
+    return(`You Win! ${playerSelection} beats ${computerSelection}`);
 }
 
 function lose(playerSelection, computerSelection) {
     computerScore++;
-    return "You Lose! " + computerSelection + " beats " + playerSelection;
+    return(`You Lose! ${computerSelection} beats ${playerSelection}`);
 }
 
 function game(){
     computerScore = 0;
     playerScore = 0;
-    for(let i = 0; i < 5; i++) {
+    for(let i = 0; i < 5; i++){
         const playerSelection = prompt("Rock, Paper, or Scissors?");
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
         console.log("Player: " + playerScore);
         console.log("Computer: " + computerScore);
     }
-    if(playerScore == computerScore){
-
-        return ("Game over, its a tie! (" + playerScore + " : " + computerScore + ")");
-    }
-    else if(playerScore > computerScore){
-        return ("Game over, human wins! (" + playerScore + " : " + computerScore + ")");
+    if(playerScore > computerScore){
+        return(`Game over, human wins! ( ${playerScore} : ${computerScore})`);
     }
     else if(playerScore < computerScore){
-        return ("Game over, the computer wins! (" + playerScore + " : " + computerScore + ")");
+        return(`Game over, the computer wins! ( ${playerScore} : ${computerScore})`);
     }
+    else return (`Its a tie! ( ${playerScore} : ${computerScore} )`);
 }
 
 console.log(game());
